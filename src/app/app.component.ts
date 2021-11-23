edc9b26d90a01914a766db8dda0b98e770c56a55
huntrontrakkr <whunt003@odu.edu>
2021-11-22 22:43:39 -0500

    Added initiaal location example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
+
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'GradTime';
+export class AppComponent  {
+  name = 'Angular';
+  public lat: any;
+  public lng: any;
+
+  public ngOnInit(): void {
+    this.getLocation();
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position: any) => {
+        if (position) {
+          console.log("Latitude: " + position.coords.latitude +
+            "Longitude: " + position.coords.longitude);
+          this.lat = position.coords.latitude;
+          this.lng = position.coords.longitude;
+          console.log(this.lat);
+          console.log(this.lat);
+        }
+      },
+        (error: any) => console.log(error));
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
 }
